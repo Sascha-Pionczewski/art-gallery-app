458bdc90c534e2fc9573c86f872d111726a6abe4
Ezequiel Angeloni <e.zeangeloni@gmail.com>
2023-03-08 15:26:27 +0100

    add fetching logic to _app

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,9 @@ import ArtPieces from "../components/ArtPieces";
 import useSWR from "swr";
 import Spotlight from "../components/Spotlight";
 
-const fetcher = (url) => fetch(url).then((res) => res.json());
-
 export default function HomePage() {
   const { data: pieces = [] } = useSWR(
-    "https://example-apis.vercel.app/api/art",
-    fetcher
+    "https://example-apis.vercel.app/api/art"
   );
 
   return (
